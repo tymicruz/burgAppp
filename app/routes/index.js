556a0110a7d1776088915a5f7c2ad7886e558029
tymicruz <tymicruz@gmail.com>
2015-08-28 22:54:03 -0700

    burger app, great start

---
+++ app/routes/index.js
@@ -0,0 +1,175 @@
+var ObjectID = require('mongodb').ObjectID;
+
+
+module.exports = function(app, db, io) {
+
+	app.get('/', function(req, res){
+		//res.send('welcome to Burgapp');
+		res.sendfile('./public/views/index.html'); //load our public/index.html file
+
+	});
+
+
+
+	app.get('/api/buns', function(req, res){
+		var buns = db.collection('buns');
+
+		buns.find({}).toArray(function(err, docs) {
+	
+			if(err) throw err;
+
+			//console.log(docs);
+			res.send(docs);
+		});
+	});
+
+	app.get('/api/meats', function(req, res){
+		var meats = db.collection('meats');
+
+		meats.find({}).toArray(function(err, docs) {
+
+			if(err) throw err;
+
+			res.send(docs);
+		});
+	});
+
+	app.get('/api/cheeses', function(req, res){
+		var cheeses = db.collection('cheeses');
+
+		cheeses.find({}).toArray(function(err, docs) {
+
+			if(err) throw err;
+
+			res.send(docs);
+		});
+	});
+
+	app.get('/api/veggies', function(req, res){
+		var veggies= db.collection('veggies');
+
+		veggies.find({}).toArray(function(err, docs) {
+
+			if(err) throw err;
+
+			res.send(docs);
+		});
+	});
+
+	app.get('/api/condos', function(req, res){
+		var condiments = db.collection('condos');
+
+		condiments.find({}).toArray(function(err, docs) {
+
+
+			if(err)
+			{
+				throw err;
+			}	
+
+			res.send(docs);
+		});
+	});
+
+	app.get('/api/others', function(req, res){
+		var others = db.collection('others');
+
+		others.find({}).toArray(function(err, docs) {
+
+			if(err)
+			{
+				throw err;
+			}	
+
+			res.send(docs);
+		});
+	});
+
+	app.post('/api/burgers', function(req, res){
+
+		var burgers = db.collection('burgers');
+		var burgerData = req.body;
+
+		burgers.insert(burgerData, function(err, inserted){
+
+			if (err) throw err;
+
+
+			console.log("created: " + JSON.stringify(inserted.ops[0]._id));
+
+			res.send(inserted.ops[0]._id);
+		});
+	});
+
+	app.get('/api/burgers/:burgerID', function(req, res){
+
+		var burgers = db.collection('burgers');
+
+		var burgerID = req.params.burgerID;
+
+		var obID = new ObjectID(burgerID);
+
+		burgers.findOne({_id: obID}, function(err, doc){
+
+			if(err)
+			{
+				res.send(400, "NOT FOUND")
+				throw err;
+			}
+
+			console.log("found: " + JSON.stringify(doc));
+			res.send(doc);
+		});
+	});
+
+	app.get('/api/burgers?:numBurgers', function(req, res){
+
+		var burgers = db.collection('burgers');
+
+		//make sure this is actually a number later on
+		//depends on client typing valid
+		console.log(req.params.numBurgers);
+		var numBurgers = parseInt(req.params.numBurgers);
+		console.log(numBurgers);
+
+		burgers.find().sort({"editDate" : -1}).limit(10).toArray(function(err, docs){
+
+			if(err)
+			{
+				res.send(400, "NOT FOUND")
+				throw err;
+			}
+
+			//console.log("found: " + JSON.stringify(docs));
+			res.send(docs);
+		});
+
+	});
+
+	app.put('/api/burgers', function(req, res){
+
+		console.log("update try:")
+		var burgers = db.collection('burgers');
+		var burger = req.body;
+		console.log(JSON.stringify(burger));
+		var burgerID = burger._id;
+
+		console.log("id is " + burgerID);
+
+		var obID = new ObjectID(burgerID);
+
+		var operator = { $set : {array : burger.array, editDate : burger.editDate}};
+
+		burgers.update({_id: obID}, operator, function(err, doc){
+
+			if(err)
+			{
+				res.send(400, "NOT FOUND")
+				throw err;
+			}
+
+			console.log("found: " + JSON.stringify(doc));
+			res.send(doc);
+		});
+	});
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+MongoClient = require('mongodb').MongoClient,
+app = express(),
+bodyParser = require('body-parser'),
+methodOverride = require('method-override'),
+io = "socket";//require('socket.io');
+
+
+
+var port = process.env.PORT || 8080;
+
+MongoClient.connect('mongodb://localhost:27017/burgapp', function(err, db) {
+	"use strict";
+
+
+	if(err) throw err;
+
+	var port = process.env.PORT || 8888;
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.json({type:'application/vnd.api+json'}));
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(methodOverride('X-HTTP-Mthod-Override'));
+
+	app.use(express.static(__dirname + '/public'));
+	
+	require('./app/routes')(app, db, io);
+
+	app.listen(port, function(){
+		console.log('Burgapp magic is happening on port ' + port);
+	});
+	
+
+});
+
+module.exports = app;
+/*8681 state route 270 pullman,wa 99163
+until 8-5:30 m-f*/
